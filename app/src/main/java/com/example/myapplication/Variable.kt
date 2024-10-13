57c90129fbb12d78ae94cd28f5eefa57715f0a7a
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 23:29:08 +0700

    Mengubah nilai integer ke variabel

--- app/src/main/java/com/example/myapplication/Variable.kt
+++ app/src/main/java/com/example/myapplication/Variable.kt
@@ -1,46 +1,11 @@
 package com.example.myapplication
 
-//kode Kotlin untuk mendemonstrasikan daftar.
-fun withoutParameter() {
-    println("== withoutParameter ==")
-    println("Hello, World!")
-}
-
-fun withParameter(name: String) {
-    println()
-    println("== withParameter ==")
-    println("Hello, $name!")
-}
-
-//named argument
-//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
-fun withNamedArgument(name: String, age: Int) {
-    println()
-    println("== withNamedArgument ==")
-    println("Hello, $name! You are $age years old.")// Memberikan nilai default
-}
-
-//default parameter value
-//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
-fun withDefaultParameter(name: String = "Indra", age: Int) {
-    println()
-    println("== withDefaultParameter ==")
-    println("Hello, $name! You are $age years old.")
-}
-
-fun withReturn(panjang: Int, lebar: Int): Int {
-    return panjang * lebar
-}
-
 fun main() {
-    withoutParameter()
-    withParameter("John")
-    withNamedArgument(name = "John", age = 30)
-    withDefaultParameter(age =25)
+    //Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
 
-    val hasil = withReturn(5, 10)
-    println("Luas: $hasil") // Menampilkan hasil luas
-
+    //Int
+    val intValue: Int = 30
+    println(intValue)
 }
-
-
