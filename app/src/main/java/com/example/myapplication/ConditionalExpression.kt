184b64d6b13d5cccc6ed71bc618922eef1176b45
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 23:38:23 +0700

    struktur if-else dalam Kotlin

---
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ====")
+
+    //if-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+}
