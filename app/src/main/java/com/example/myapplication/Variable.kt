70e89065597bc65527400b1d789f31f06f0b273c
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 23:30:45 +0700

    Menambahkan tipe data Float dan Char

--- app/src/main/java/com/example/myapplication/Variable.kt
+++ app/src/main/java/com/example/myapplication/Variable.kt
@@ -8,4 +8,12 @@ fun main() {
     //Int
     val intValue: Int = 30
     println(intValue)
+
+    //Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
 }
