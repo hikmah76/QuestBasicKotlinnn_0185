41499d4a59f429c40891e105174be7ec5ce4e766
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 23:58:47 +0700

    menggunakan while

--- app/src/main/java/com/example/myapplication/ConditionalExpression.kt
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -45,4 +45,16 @@ fun Perulangan() {
     for (car in cars) {
         println("Car type: $car")
     }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
 }
