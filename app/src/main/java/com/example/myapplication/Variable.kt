a76318dc6f269e06112327c10112f017413b1322
hikmah76 <nur.hikmah190504@gmail.com>
2024-10-13 23:17:01 +0700

    Menambahkan nilai default

--- app/src/main/java/com/example/myapplication/Variable.kt
+++ app/src/main/java/com/example/myapplication/Variable.kt
@@ -20,4 +20,12 @@ fun withNamedArgument(name: String, age: Int) {
     println("Hello, $name! You are $age years old.")// Memberikan nilai default
 }
 
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
 
